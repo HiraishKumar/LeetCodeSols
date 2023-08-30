a8a5952b1b6aabc6ac34b8429b6b6fcc44cb3dff
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-08-30 23:21:16 +0530

    i am so confused

--- Daily Temperatures.py
+++ Daily Temperatures.py
@@ -1,13 +1,35 @@
 temperatures = [73,74,75,71,69,72,76,73]
-
+'''[1,1,4,2,1,1,0,0]'''
 def DailyTemprateures(temperatures:list[int])->list[int]:
-    stack=[]
-    results=[]
-    for i in temperatures:
-        duration=0
-        if stack and i > stack[0]:            
-            while stack and i > stack[0]:
-                stack.pop()
-                duration+=1
-                
+    stack = []
+    ans = [0] * len(temperatures)
+    for index in range(len(temperatures)):
+        while stack and temperatures[index] > temperatures[stack[-1]]:
+            end=stack.pop()
+            ans[end] = index - end
+        stack.append(index)
+        
+    return ans
 print(DailyTemprateures(temperatures))
+
+
+
+    # stack=[temperatures[0]]
+    # results=[]
+    # for i in range(1,len(temperatures)):
+    #     index=i
+    #     while temperatures[index] < stack[0]:
+    #         if index > len(temperatures)-2:
+    #             results.append(0)
+    #             break
+    #         else:
+    #             stack.append(temperatures[index])
+    #             index+=1
+    #     results.append(len(stack))
+    #     stack.clear()
+    #     stack.append(temperatures[i])
+        
+    # results.pop()
+    # results.append(0)
+    # return results
+                
