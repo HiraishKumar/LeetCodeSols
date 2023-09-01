056b695c51b21dfe548193e96f94bee6502eadb4
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-09-02 00:32:05 +0530

    restructuring

---
+++ 3SUM.py
@@ -0,0 +1,28 @@
+nums = [-2,0,1,1,2]
+'''
+[[-2,0,2],[-2,1,1]]
+'''
+
+def threeSum(nums: list[int]) -> list[list[int]]:
+    nums.sort()
+    s=set()
+    for i in range(len(nums)):
+        j=i+1
+        k=len(nums)-1
+        while j < k :
+            target = nums[i] + nums[j] + nums[k]
+            if target==0:                
+                input=(nums[i],nums[j],nums[k])
+                s.add(input)  
+                j+=1
+                k-=1
+          
+            elif target > 0:
+                k-=1
+            elif target < 0:
+                j+=1
+    return [list(i) for i in s]    
+          
+print(threeSum(nums))    
+                
+            
