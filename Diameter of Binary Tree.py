6b8f7ec2c1b4121f44ad9b3a147d77b607c34041
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-11-07 23:23:36 +0530

    LeetCode

---
+++ Diameter of Binary Tree.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def diameterOfBinaryTree(root:[TreeNode]) -> int:
+    res=[0]
+    def dfs(root):
+        if not root:
+            return -1
+        left=dfs(root.left)
+        right=dfs(root.right)
+        res[0]=max(left+right+2,res[0])
+        return 1+max(left,right)
+    dfs(root)
+    return res[0]
+
+        
