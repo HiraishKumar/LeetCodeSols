b591a6553b9e9c8640aef5f81f66187444f99edf
Killy <87876086+KillyDaviel@users.noreply.github.com>
2024-01-20 00:14:22 +0530

    CarFleet

--- carfleet.py
+++ carfleet.py
@@ -1,5 +1,5 @@
 class Solution:
-    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
         pair=[[p,s] for p,s in zip(position,speed)]
         stack=[]
         for p,s in sorted(pair)[::-1]:
