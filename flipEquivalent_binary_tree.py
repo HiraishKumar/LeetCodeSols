73976cd10c2a8123c695021c2d6f919d3c2afc4b
HiraishKumar <heraishkumar123@gmail.com>
2024-10-24 23:57:15 +0530

    flip equivalent bnary tree

---
+++ flipEquivalent_binary_tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def flipEquiv(self, root1, root2):
+        
+        def checker(node_1, node_2):
+            if not node_1 and not node_2:
+                return True
+            if not node_1 or not node_2 or node_1.val != node_2.val:
+                return False
+            return ((checker(node_1.left, node_2.left) or checker(node_1.left, node_2.right)) and
+                    (checker(node_1.right, node_2.right) or checker(node_1.right, node_2.left)))
+        
+        return checker(root1, root2)
